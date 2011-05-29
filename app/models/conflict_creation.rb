c543e218a1f9a7c7fc3dd6e0e58b81817f68c883
Chirantan Mitra <chirantan.mitra@gmail.com>
2011-05-30 01:19:18 +0530

    Model knows how to find out 'got_ya'

--- app/models/conflict_creation.rb
+++ app/models/conflict_creation.rb
@@ -1,7 +1,7 @@
 class ConflictCreation
   include ActiveModel::Validations
 
-  attr_accessor :op, :dn, :ocn
+  attr_accessor :op, :dn, :ocn, :got_ya
 
   validates_presence_of :op
   validate :dn_or_ocn_is_present
@@ -10,9 +10,16 @@ class ConflictCreation
     @op  = options[:op]
     @dn  = options[:dn]
     @ocn = options[:ocn]
+
+    assign_got_ya
   end
 
   private
+
+  def assign_got_ya
+    which_1 = dn || ocn
+    @got_ya = CrazyStuff.find_by_which_1(which_1)
+  end
 
   def dn_or_ocn_is_present
     if not(dn.present? or ocn.present?)
--- test/unit/conflict_creation_test.rb
+++ test/unit/conflict_creation_test.rb
@@ -1,29 +1,52 @@
 require 'test_helper'
 
 class ConflictCreationTest < ActiveSupport::TestCase
-  test "should have an error when op is absent" do
+  test "should have an error when 'op' is absent" do
     conflict_creation = Factory.build :conflict_creation, :op => nil
 
     assert conflict_creation.invalid?
     assert conflict_creation.errors[:op].present?
   end
 
-  test "should have an error when dn and ocn are absent" do
+  test "should have an error when 'dn' and 'ocn' are absent" do
     conflict_creation = Factory.build :conflict_creation, :dn => '', :ocn => ''
 
     assert conflict_creation.invalid?
     assert conflict_creation.errors[:base].present?
   end
 
-  test "should be valid via dn" do
+  test "should be valid via 'dn'" do
     conflict_creation = Factory.build :conflict_creation, :dn => 'dn'
 
     assert conflict_creation.valid?
   end
 
-  test "should be valid via ocn" do
+  test "should be valid via 'ocn'" do
     conflict_creation = Factory.build :conflict_creation, :ocn => 'ocn'
 
     assert conflict_creation.valid?
+  end
+
+  test "should properly assign 'got_ya' via 'dn'" do
+    dn                = dn
+    crazy_stuff       = Factory :crazy_stuff, :which_1 => dn
+    conflict_creation = Factory.build :conflict_creation, :dn => dn
+
+    assert_equal crazy_stuff, conflict_creation.got_ya
+  end
+
+  test "should properly assign 'got_ya' via 'ocn'" do
+    ocn               = ocn
+    crazy_stuff       = Factory :crazy_stuff, :which_1 => ocn
+    conflict_creation = Factory.build :conflict_creation, :ocn => ocn
+
+    assert_equal crazy_stuff, conflict_creation.got_ya
+  end
+
+  test "should not have 'got_ya' when 'dn' and 'ocn' don't match" do
+    crazy_stuff       = Factory :crazy_stuff, :which_1 => 'something else'
+    conflict_creation = Factory.build :conflict_creation, :dn => 'dn', :ocn => 'ocn'
+
+    assert_equal nil, conflict_creation.got_ya
   end
 end
