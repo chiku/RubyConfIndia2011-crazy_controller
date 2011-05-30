80f835aca2c4323b5ecba0c652c4553a304634cc
Chirantan Mitra <chirantan.mitra@gmail.com>
2011-05-30 23:00:06 +0530

    Delete unnecessary controller code. Model validates presence of 'got_ya'

--- app/controllers/conflict_creations_controller.rb
+++ app/controllers/conflict_creations_controller.rb
@@ -8,8 +8,6 @@ class ConflictCreationsController < ApplicationController
     which_1 = params[:dn] || params[:ocn]
     got_ya = CrazyStuff.find_by_which_1(which_1)
 
-    render_unprocessable_entity("Really") and return unless got_ya
-
     got_ya.conflict_with params[:op]
 
     if got_ya.conflicted?
