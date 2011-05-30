09655330e7db7fa2c89a7726712bf03544f25e86
Chirantan Mitra <chirantan.mitra@gmail.com>
2011-05-30 22:28:02 +0530

    Remove factory for ConflictCreation as it seems to be flaky. The attributes are not properly populated.

--- test/factories.rb
+++ test/factories.rb
@@ -1,7 +1,3 @@
 Factory.define :crazy_stuff do |f|
   f.sequence(:which_1) { |n| "which one ? #{n}" }
 end
-
-Factory.define :conflict_creation do |f|
-  f.sequence(:op) { |n| "op #{n}" }
-end
--- test/unit/conflict_creation_test.rb
+++ test/unit/conflict_creation_test.rb
@@ -2,27 +2,27 @@ require 'test_helper'
 
 class ConflictCreationTest < ActiveSupport::TestCase
   test "should have an error when 'op' is absent" do
-    conflict_creation = Factory.build :conflict_creation, :op => nil
+    conflict_creation = ConflictCreation.new :op => nil
 
     assert conflict_creation.invalid?
     assert conflict_creation.errors[:op].present?
   end
 
   test "should have an error when 'dn' and 'ocn' are absent" do
-    conflict_creation = Factory.build :conflict_creation, :dn => '', :ocn => ''
+    conflict_creation = ConflictCreation.new :dn => '', :ocn => ''
 
     assert conflict_creation.invalid?
     assert conflict_creation.errors[:base].present?
   end
 
   test "should be valid via 'dn'" do
-    conflict_creation = Factory.build :conflict_creation, :dn => 'dn'
+    conflict_creation = ConflictCreation.new :dn => 'dn', :op => 'op'
 
     assert conflict_creation.valid?
   end
 
   test "should be valid via 'ocn'" do
-    conflict_creation = Factory.build :conflict_creation, :ocn => 'ocn'
+    conflict_creation = ConflictCreation.new :ocn => 'ocn', :op => 'op'
 
     assert conflict_creation.valid?
   end
@@ -30,7 +30,7 @@ class ConflictCreationTest < ActiveSupport::TestCase
   test "should properly assign 'got_ya' via 'dn'" do
     dn                = dn
     crazy_stuff       = Factory :crazy_stuff, :which_1 => dn
-    conflict_creation = Factory.build :conflict_creation, :dn => dn
+    conflict_creation = ConflictCreation.new :dn => dn
 
     assert_equal crazy_stuff, conflict_creation.got_ya
   end
@@ -38,14 +38,14 @@ class ConflictCreationTest < ActiveSupport::TestCase
   test "should properly assign 'got_ya' via 'ocn'" do
     ocn               = ocn
     crazy_stuff       = Factory :crazy_stuff, :which_1 => ocn
-    conflict_creation = Factory.build :conflict_creation, :ocn => ocn
+    conflict_creation = ConflictCreation.new :ocn => ocn
 
     assert_equal crazy_stuff, conflict_creation.got_ya
   end
 
   test "should not have 'got_ya' when 'dn' and 'ocn' don't match" do
     crazy_stuff       = Factory :crazy_stuff, :which_1 => 'something else'
-    conflict_creation = Factory.build :conflict_creation, :dn => 'dn', :ocn => 'ocn'
+    conflict_creation = ConflictCreation.new :dn => 'dn', :ocn => 'ocn'
 
     assert_equal nil, conflict_creation.got_ya
   end
